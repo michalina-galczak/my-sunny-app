3e1b7713f91d107f92dbbaefa519e6ee3c244093
michalina-galczak <70964363+michalina-galczak@users.noreply.github.com>
2020-11-01 17:30:28 +0000

    Added real data for humidity, wind & sky

--- src/index.js
+++ src/index.js
@@ -68,6 +68,12 @@ function showTemperature(response) {
   h2.innerHTML = `${temperature}°C`;
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let skyElement = document.querySelector("#sky");
+  skyElement.innerHTML = response.data.weather[0].main;
 }
 
 function showCityTemp(city) {
