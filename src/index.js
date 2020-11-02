c9f630ec68d630368795ef1a0d2159ce7a8a441b
michalina-galczak <70964363+michalina-galczak@users.noreply.github.com>
2020-11-02 22:09:38 +0000

    Created forecast & timezone for forecast

--- src/index.js
+++ src/index.js
@@ -3,21 +3,24 @@ let statsDiv = document.querySelector("#stats");
 weatherIcon.style.visibility = "hidden";
 statsDiv.style.visibility = "hidden";
 
-function formatDate(timestamp, timezone) {
+function getTimezoneDate(timestamp, timezone) {
   let timeNow = new Date(timestamp);
   let offset = timeNow.getTimezoneOffset() * 60000;
   let utc = timestamp + offset;
   let cityTime = utc + (1000 * timezone);
-  let cityDate = new Date(cityTime);
+
+  return new Date(cityTime);
+}
+
+function formatDate(timestamp, timezone) {
+  let cityDate = getTimezoneDate(timestamp, timezone);
   let hours = cityDate.getHours();
 
-
-  //let hours = timeNow.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
+
   let minutes = cityDate.getMinutes();
-  //let minutes = timeNow.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
@@ -94,10 +97,49 @@ function showTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000, response.data.timezone);
 }
 
+function formatHours(timestamp, timezone) {
+  let cityDate = getTimezoneDate(timestamp, timezone);
+  let hours = cityDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  
+  let minutes = cityDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+return `${hours}:${minutes}`
+}
+
+
 function showCityTemp(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=de721be4f431ff99a9769e3b29d705a6&units=metric`;
-
   axios.get(apiUrl).then(showTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=de721be4f431ff99a9769e3b29d705a6&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index <5; index++) {
+  forecast = response.data.list[index];
+  forecastElement.innerHTML += `
+  <div class="col-2">
+  <h5>
+    ${formatHours(forecast.dt * 1000, response.data.city.timezone)}
+  </h5>
+  <img 
+  src = "https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+  />
+  <p>
+    <strong>${Math.round(forecast.main.temp_max)}°</strong>  |  ${Math.round(forecast.main.temp_min)}°
+  </p>
+</div>
+  `
+}
 }
 
 function showPosition(position) {
@@ -107,3 +149,4 @@ function showPosition(position) {
 
   axios.get(apiUrl).then(showTemperature);
 }
+
